ba1d8e98dada739b1f6437732acee03bae52470b
Robert Varela <rvarela@utexas.edu>
2022-04-19 17:09:44 -0500

    Updated middleware file

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-// Force-redirect every HTTP request to HTTPS
-export function middleware(req: NextRequest) {
+function forceHTTPS(req: NextRequest) {
   const host = req.headers.get('host');
 	const url = req.nextUrl.pathname;
   if (
@@ -9,9 +8,30 @@ export function middleware(req: NextRequest) {
     req.headers.get('x-forwarded-proto') !== 'https' && 
     !host.includes('localhost')
   ) {
-    return NextResponse.redirect(
-      `https://${host}${url}`,
-      301
-    );
+    return NextResponse.redirect(`https://${host}${url}`,301);
   }
+}
+
+function redirectWWWToNonWWW(req: NextRequest) {
+  const host = req.headers.get('host');
+  const url = req.nextUrl.pathname;
+  const wwwRegex = /^www\./;
+  if (wwwRegex.test(host) && !host.includes('localhost')) {
+    const newHost = host.replace(wwwRegex, '');
+    return NextResponse.redirect(`https://${newHost}${url}`, 301);
+  }
+}
+
+function processMiddlewareFunctions(req: NextRequest, middlewareFns: Function[]) {
+  for (const middlewareFn of middlewareFns) {
+    const fnResponse = middlewareFn(req);
+    if (fnResponse) {
+      return fnResponse;
+    }
+  }
+  return NextResponse.next();
+}
+
+export function middleware(req: NextRequest) {
+  return processMiddlewareFunctions(req, [ forceHTTPS, redirectWWWToNonWWW ]);
 }
