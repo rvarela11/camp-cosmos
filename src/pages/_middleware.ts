369419ba5e03a4b4954277491ab0473ef19e9b30
Robert Varela <rvarela@utexas.edu>
2022-04-19 17:20:19 -0500

    Removed middleware WWW

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-function forceHTTPS(req: NextRequest) {
+export function middleware(req: NextRequest) {
   const host = req.headers.get('host');
 	const url = req.nextUrl.pathname;
   if (
@@ -8,30 +8,6 @@ function forceHTTPS(req: NextRequest) {
     req.headers.get('x-forwarded-proto') !== 'https' && 
     !host.includes('localhost')
   ) {
-    return NextResponse.redirect(`https://${host}${url}`,301);
+    return NextResponse.redirect(`https://${host}${url}`, 301);
   }
-}
-
-function redirectWWWToNonWWW(req: NextRequest) {
-  const host = req.headers.get('host');
-  const url = req.nextUrl.pathname;
-  const wwwRegex = /^www\./;
-  if (wwwRegex.test(host) && !host.includes('localhost')) {
-    const newHost = host.replace(wwwRegex, '');
-    return NextResponse.redirect(`https://${newHost}${url}`, 301);
-  }
-}
-
-function processMiddlewareFunctions(req: NextRequest, middlewareFns: Function[]) {
-  for (const middlewareFn of middlewareFns) {
-    const fnResponse = middlewareFn(req);
-    if (fnResponse) {
-      return fnResponse;
-    }
-  }
-  return NextResponse.next();
-}
-
-export function middleware(req: NextRequest) {
-  return processMiddlewareFunctions(req, [ forceHTTPS, redirectWWWToNonWWW ]);
 }
