c50d9b3c21d59a1b513b8a4650cefca50aae6ab7
Robert Varela <rvarela@utexas.edu>
2022-04-19 20:38:35 -0500

    Code clean up

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Header from '../header';
 
 const Layout = ({ children }) => (
-	<>
-		<Header />
-		{children}
-	</>
+  <>
+    <Header />
+    {children}
+  </>
 );
 
 export default Layout;
