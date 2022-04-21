73d1b4963cf38c058ad367a785d5ea3c51820ec4
Robert Varela <rvarela@utexas.edu>
2022-04-21 18:47:05 -0500

    task: Update folder structure (#9)
    * Moved folders around
    
    * Code clean up
    
    * Added TailwindCSS
    
    * Removed SASS

--- src/pages/galaxy/index.js
+++ src/pages/galaxy/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Link from 'next/link'
-import Image from 'next/image'
-
-import Layout from '../components/layout';
+import Link from 'next/link';
+import Image from 'next/image';
+import Layout from '../../components/layout';
 
 const GalaxyPage = () => (
   <Layout>
