4555d8eb481734ebf2bdbcd1b6f557fae137fbc7
Robert Varela <rvarela@utexas.edu>
2022-04-21 16:25:01 -0500

    Added TailwindCSS

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './header.module.scss';
 
-const Header = () => <header className={styles.header}>HEADER</header>;
+const Header = () => (
+  <header className={styles.header}>
+    <h1 className="text-3xl font-bold underline">
+      Header
+    </h1>
+  </header>
+);
 
 export default Header;
