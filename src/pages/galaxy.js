023e0d1fedef8594e223b2a05577a82c1809a2a4
Robert Varela <rvarela@utexas.edu>
2022-04-20 19:54:16 -0500

    Added galaxy--description__title

--- src/pages/galaxy.js
+++ src/pages/galaxy.js
@@ -4,7 +4,9 @@ import styles from '../styles/galaxy.module.scss';
 
 const GalaxyPage = () => (
   <Layout>
-    <div className={styles.galaxy}>
+    <div className={styles['galaxy--background']} />
+    <div className={styles['galaxy--description']}>
+      <h2 className={styles['galaxy--description__title']}>Welcome to Camp Cosmos</h2>
     </div>
   </Layout>
 );
