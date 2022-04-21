d0f048220f206acd25696b1a81b2a18a21bcb469
Alessandra Davila <adavila91@gmail.com>
2022-04-20 23:45:27 -0500

    feat(header): add header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Twitter } from './icons/twitter';
+import { Discord } from './icons/discord';
+import { Instagram } from './icons/instagram';
 import styles from './header.module.scss';
 
 const Header = () => (
   <header className={styles.header}>
-    <div></div>
-    <img src='images/camp_cosmos_logo.svg' alt='Camp Cosmos' />
+    <div className={styles.logo}>
+      <img src='images/camp_cosmos_logo.svg' alt='Camp Cosmos' />
+    </div>
+    <div className={styles.nav}>
+      <div className={styles.buttonGroup} aria-label="nav menu">
+        <button type="button" className={styles.button}>THE GALAXY</button>
+        <button type="button" className={styles.button}>FIRESIDE</button>
+        <button type="button" className={styles.button}>SHOP</button>
+      </div>
+    </div>
+    <div className={styles.socials}>
+      <div className={styles.buttonGroup} aria-label="socials menu">
+        <button type="button" className={styles.button}><Twitter /></button>
+        <button type="button" className={styles.button}><Instagram /></button>
+        <button type="button" className={styles.button}><Discord /></button>
+      </div>
+    </div>
   </header>
 );
 
