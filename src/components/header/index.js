8cabffecb64fac80600f966706ffc5cc7b21f3e9
Robert Varela <rvarela@utexas.edu>
2022-04-24 11:40:10 -0500

    Updated Nav bar routes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,37 +1,45 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import classNames from 'classnames';
 import { Twitter } from './icons/twitter';
 import { Discord } from './icons/discord';
 import { Instagram } from './icons/instagram';
+import { NAV_ROUTES, ROUTES } from '../../../src/utils/routes';
 
-const Header = () => (
-  <header className="header">
-    <div className="header--logo">
-      <Link href="/">
-        <img src='images/camp_cosmos_logo.svg' alt='Camp Cosmos' />
-      </Link>
-    </div>
-    <div className="header--nav">
-      <div className="h-button--group" aria-label="nav menu">
-        <Link href="/galaxy">
-          <button type="button" className="h-button">THE GALAXY</button>
-        </Link>
-        <Link href="/fireside">
-          <button type="button" className="h-button">FIRESIDE</button>
-        </Link>
-        <Link href="/shop">
-          <button type="button" className="h-button">SHOP</button>
+const Header = () => {
+  const { pathname } = useRouter();
+
+  return (
+    <header className="header">
+      <div className="header--logo">
+        <Link href={ROUTES.HOME}>
+          <img src="images/camp_cosmos_logo.svg" alt="Camp Cosmos" />
         </Link>
       </div>
-    </div>
-    <div className="header--socials">
-      <div className="h-button--group" aria-label="socials menu">
-        <button type="button" className="h-button"><Twitter /></button>
-        <button type="button" className="h-button"><Instagram /></button>
-        <button type="button" className="h-button"><Discord /></button>
+      <div className={classNames("header--nav", { ["header--nav-hide"]: pathname === ROUTES.HOME })}>
+        <div className="h-button--group" aria-label="nav menu">
+          {
+            Object.entries(NAV_ROUTES).map(([label, route]) => (
+              <Link href={route}>
+                <button
+                  type="button"
+                  className={classNames("h-button", { ["h-button-selected"]: pathname === route })}
+                >{label}</button>
+              </Link>
+            ))
+          }
+        </div>
       </div>
-    </div>
-  </header>
-);
+      <div className="header--socials">
+        <div className="h-button--group" aria-label="socials menu">
+          <button type="button" className="h-button"><Twitter /></button>
+          <button type="button" className="h-button"><Instagram /></button>
+          <button type="button" className="h-button"><Discord /></button>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
---
+++ src/pages/faq/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+const FAQ = () => (
+  <Layout>
+    <h2>FAQ</h2>
+  </Layout>
+);
+
+export default FAQ;
