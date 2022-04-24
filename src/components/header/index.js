f3e9ac0c4a7b4b0822df6a5bead71b2b95fe29ec
Alessandra Davila <adavila91@gmail.com>
2022-04-23 23:37:25 -0500

    Revert "feat: optimize images (#13)"
    This reverts commit f052bd75f38a5dee231927fbf6d59821f38f0f70.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import { Twitter } from './icons/twitter';
 import { Discord } from './icons/discord';
 import { Instagram } from './icons/instagram';
-import CampCosmosLogo from '../../../public/images/camp-cosmos-logo.svg';
 
 const Header = () => (
   <header className="header">
     <div className="header--logo">
       <Link href="/">
-        <Image
-          src={CampCosmosLogo}
-          alt="Camp Cosmos logo"
-        />
+        <img src='images/camp_cosmos_logo.svg' alt='Camp Cosmos' />
       </Link>
     </div>
     <div className="header--nav">
