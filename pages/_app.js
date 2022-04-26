5bcc7c7c17f6b3df3614b3c6d561a6a54ae5e10b
Robert Varela <rvarela@utexas.edu>
2022-04-25 21:56:30 -0500

    Added MUI. Remove tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import createEmotionCache from '../utility/createEmotionCache';
+import mainTheme from '../styles/theme/main';
 import '../styles/globals.css';
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const clientSideEmotionCache = createEmotionCache();
+
+const App = ({ Component, emotionCache = clientSideEmotionCache, pageProps }) => (
+  <CacheProvider value={emotionCache}>
+    <ThemeProvider theme={mainTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </CacheProvider>
+);
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default App;
