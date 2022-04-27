86e48a587d8c35adfb93889ef4987a7cba05f310
Robert Varela <rvarela@utexas.edu>
2022-04-27 18:24:57 -0500

    Fixed lint error

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,8 +9,8 @@ const Home = () => (
   <Layout>
     <Head>
       <meta charset="utf-8" />
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <meta property="og:site_name" content="Camp Cosmos" />
       <meta property="og:url" content="https://www.campcosmos.io" />
