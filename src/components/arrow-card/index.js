b15fa56bfa00a3693d2af2060d23b859193fa733
Robert Varela <rvarela@utexas.edu>
2022-04-27 15:56:49 -0500

    Code clean up

--- src/components/arrow-card/index.js
+++ src/components/arrow-card/index.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import Arrow from '../arrow';
 
 const ArrowCard = ({ className, children }) => (
-	<div className={`${className} arrow-card`}>
-		{children}
-		<div>
-			<Arrow className="arrow--inactive" />
-			<Arrow className="arrow--active arrow-sliding" />
-		</div>
-	</div>
+  <div className={`${className} arrow-card`}>
+    {children}
+    <div>
+      <Arrow className="arrow--inactive" />
+      <Arrow className="arrow--active arrow-sliding" />
+    </div>
+  </div>
 );
 
 ArrowCard.propTypes = {
