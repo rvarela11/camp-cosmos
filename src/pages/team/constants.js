c886b9f3b560c053c49c0671b8bbabaed37df513
Robert Varela <rvarela@utexas.edu>
2022-04-28 17:16:01 -0500

    Added Tabs

---
+++ src/pages/team/constants.js
@@ -0,0 +1,14 @@
+export const TABS = [
+  {
+    id: 0,
+    label: "Alexandra 'Zutto'"
+  },
+  {
+    id: 1,
+    label: "West Coast"
+  },
+  {
+    id: 2,
+    label: "Core Team"
+  },
+];
--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import Layout from '../../components/layout';
+import { TABS } from './constants';
 
-const Team = () => (
-  <Layout>
-    <h2>Team</h2>
-  </Layout>
-);
+// NOTE - TabPanel will be replaced by the actual tab pages
+const TabPanel = (props) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+const Team = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Layout>
+      <h2>Team</h2>
+      <Box sx={{ width: '100%' }}>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+        <Box sx={{
+          backgroundColor: 'rgba(0,0,0,0.3)',
+          position: 'fixed',
+          width: '100%',
+          bottom: 0,
+        }}>
+          <Tabs
+            aria-label="Team page tabs"
+            className="team--tabs"
+            onChange={handleChange}
+            TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+            value={value}
+          >
+            {
+              TABS.map(({ id, label }) => (
+                <Tab
+                  key={id}
+                  className="team--tabs__tab"
+                  disableRipple
+                  label={label}
+                />
+              ))
+            }
+          </Tabs>
+        </Box>
+      </Box>
+    </Layout>
+  );
+};
 
 export default Team;
