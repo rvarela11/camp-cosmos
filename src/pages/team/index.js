1c496730f87baecdb8ff3ba8b08951bb236fc7bf
Robert Varela <rvarela@utexas.edu>
2022-04-28 21:03:13 -0500

    Added Alexandra tab

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -3,28 +3,14 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Alexandra from './tabs/alexandra';
 import Layout from '../../components/layout';
 import { TABS } from './constants';
 
-// NOTE - TabPanel will be replaced by the actual tab pages
-const TabPanel = (props) => {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+const TabContent = {
+  0: <Alexandra />,
+  1: "Item 2",
+  2: "Item 3",
 };
 
 const Team = () => {
@@ -36,22 +22,14 @@ const Team = () => {
 
   return (
     <Layout>
-      <h2>Team</h2>
-      <Box sx={{ width: '100%' }}>
-        <TabPanel value={value} index={0}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          Item Three
-        </TabPanel>
+      <Box className="team" sx={{ width: '100%' }}>
+        <div className="team--tabs-content">{ TabContent[value]}</div>
         <Box sx={{
           backgroundColor: 'rgba(0,0,0,0.3)',
           position: 'fixed',
           width: '100%',
           bottom: 0,
+          left: 0,
         }}>
           <Tabs
             aria-label="Team page tabs"
