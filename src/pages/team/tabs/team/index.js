a0f252250e763cc8ba049867a3120694f0512b05
Robert Varela <rvarela@utexas.edu>
2022-04-29 17:54:50 -0500

    Added on hover

--- src/pages/team/tabs/team/index.js
+++ src/pages/team/tabs/team/index.js
@@ -1,40 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Twitter } from '../../../../components/header/icons/twitter';
+import TeamCardFront from './team-card-front';
+import TeamCardBack from './team-card-back';
 
 const TeamTab = ({ members, title }) => (
   <>
     <h2 className="team-tab-title">{title}</h2>
     <div className="team-tab-grid">
       {
-        members.map(({ member, name, position, social }, index) => {
-          return ( 
-            <div key={index} className="team-tab-grid__card">
-              <img className="team-tab-grid__card-image" src={`/images/team-members/${member}.png`} alt={`${member} image`} />
-              <div className="team-tab-grid__card-role">
-                <p>{name}</p>
-                <p>{position}</p>
-              </div>
-              <div className="team-tab-grid__card-social">
-                <p>{social}</p>
-                <Twitter />
-              </div>
-            </div>
-          );
-        })
+        members.map((props, index) => (
+          <div key={index} className="team-tab-grid__card">
+            <TeamCardFront {...props} />
+            <TeamCardBack {...props} />
+          </div>
+        ))
       }
     </div>
   </>
 );
-
-TeamTab.propTypes = {
-  members: PropTypes.arrayOf(PropTypes.shape({
-    member: PropTypes.string,
-    name: PropTypes.string,
-    position: PropTypes.string,
-    social: PropTypes.string,
-  })).isRequired,
-  title: PropTypes.string.isRequired,
-};
 
 export default TeamTab;
---
+++ src/pages/team/tabs/team/team-card-back/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Twitter } from '../../../../../components/header/icons/twitter';
+
+const TeamCardBack = ({ description, member, name, position, social }) => (
+	<div className="team-tab-grid__card-back">
+		<div className="team-tab-grid__card-role">
+			<div className="team-tab-grid__card-role-image">
+				<img className="team-tab-grid__card-image" src={`/images/team-members/${member}.png`} alt={`${member} image`} />
+			</div>
+			<div className="team-tab-grid__card-role-description">
+				<p>{name}</p>
+				<p>{position}</p>
+			</div>
+		</div>
+		<div className="team-tab-grid__card-description">
+			<p>{description}</p>
+		</div>
+		<div className="team-tab-grid__card-social">
+			<p>{social}</p>
+			<Twitter />
+		</div>
+	</div>
+);
+
+TeamCardBack.propTypes = {
+	description: PropTypes.string.isRequired,
+  member: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	position: PropTypes.string.isRequired,
+	social: PropTypes.string.isRequired,
+};
+
+export default TeamCardBack;
