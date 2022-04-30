134407a7b9390a2777945e5438e96b3f7eebf441
Robert Varela <rvarela@utexas.edu>
2022-04-30 15:21:08 -0500

    Code clean up

--- src/components/header/mobile-nav/index.js
+++ src/components/header/mobile-nav/index.js
@@ -12,62 +12,46 @@ import { Discord } from '../icons/discord';
 import { Instagram } from '../icons/instagram';
 import { NAV_ROUTES, ROUTES } from '../../../utils/routes';
 
-const MobileNav = ({ handleDrawerToggle, isMobileMenuOpen }) => {
-
-	const handleOnClose = () => {
-    handleDrawerToggle();
-  };
-
-	return (
-		<Box component="nav">
-			<Drawer
-				anchor="right"
-				className="mobile-nav"
-				variant="temporary"
-				open={isMobileMenuOpen}
-				ModalProps={{ keepMounted: true }}
-			>
-				<div className="mobile-nav__close">
-					<Link href={ROUTES.HOME}>
-						<a>
-							<img src="images/camp-cosmos-logo.svg" alt="Camp Cosmos" />
-						</a>
-					</Link>
-					<IconButton
-						color="inherit"
-						aria-label="open menu"
-						edge="start"
-						onClick={handleDrawerToggle}
-						>
-						<CloseIcon />
-					</IconButton>
-				</div>
-				<div className="mobile-nav__nav">
-					{
-            Object.entries(NAV_ROUTES).map(([label, route]) => (
-              <Link key={route} href={route}>
-                <a>
-                  <button
-                    type="button"
-										className="mobile-nav__nav-button"
-                  >
-                    {label}
-                  </button>
-                </a>
-              </Link>
-            ))
-          }
-					<button aria-label="Twitter icon button" className="mobile-nav__nav-button">Twitter <Twitter /></button>
-					<button aria-label="Instagram icon button" className="mobile-nav__nav-button">Instagram <Instagram /></button>
-					<button aria-label="Discord icon button" className="mobile-nav__nav-button">Discord <Discord /></button>
-				</div>
-			</Drawer>
-		</Box>
-	)
-}
+const MobileNav = ({ handleDrawerToggle, isMobileMenuOpen }) => (
+  <Box component="nav">
+    <Drawer
+      anchor="right"
+      className="mobile-nav"
+      open={isMobileMenuOpen}
+      variant="temporary"
+      ModalProps={{ keepMounted: true }}
+    >
+      <div className="mobile-nav__close">
+        <Link href={ROUTES.HOME}>
+         <a><img src="images/camp-cosmos-logo.svg" alt="Camp Cosmos" /></a>
+        </Link>
+        <IconButton
+          aria-label="open menu"
+          color="inherit"
+          edge="start"
+          onClick={handleDrawerToggle}
+        >
+          <CloseIcon />
+        </IconButton>
+      </div>
+      <div className="mobile-nav__nav">
+        {
+          Object.entries(NAV_ROUTES).map(([label, route]) => (
+            <Link key={route} href={route}>
+              <a><button aria-label={`${label} button`} className="mobile-nav__nav-button" type="button" >{label}</button></a>
+            </Link>
+          ))
+        }
+        <button aria-label="Twitter icon button" className="mobile-nav__nav-button" type="button">Twitter <Twitter /></button>
+        <button aria-label="Instagram icon button" className="mobile-nav__nav-button" type="button">Instagram <Instagram /></button>
+        <button aria-label="Discord icon button" className="mobile-nav__nav-button" type="button">Discord <Discord /></button>
+      </div>
+    </Drawer>
+  </Box>
+);
 
 MobileNav.propTypes = {
-	handleDrawerToggle: PropTypes.func.isRequired,
+  handleDrawerToggle: PropTypes.func.isRequired,
   isMobileMenuOpen: PropTypes.bool.isRequired,
 };
 
