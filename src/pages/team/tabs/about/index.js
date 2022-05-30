b536122c5f5f7fd5c7275850101bc7ee0b87cb82
Roberto Varela <rvarela@utexas.edu>
2022-05-30 14:23:33 -0500

    Added tabs.scss

--- src/pages/team/tabs/about/index.js
+++ src/pages/team/tabs/about/index.js
@@ -3,14 +3,14 @@ import Image from 'next/image';
 import loreImage from '../../../../../public/images/camp-cosmos-lore.png';
 
 const AboutTab = () => (
-  <div className="about-tab">
-    <div className="about-tab--description page-layout">
-     <h2 className="about-tab--description__title">Who We Are <span>& What We Do</span></h2>
-      <div className="about-tab--description__content">
+  <div className="team-tab">
+    <div className="team-tab--description page-layout">
+     <h2 className="team-tab--description__title">Who We Are <span>& What We Do</span></h2>
+      <div className="team-tab--description__content">
         <p>The Camp Cosmos team is made up of artists, investors, and visionaries across various fields of skill and expertise. We believe the future of the internet is decentralized, and we are committed to pushing the boundaries of web3.</p>
       </div>
     </div>
-    <div className="about-tab--background">
+    <div className="team-tab--background">
         <Image
           src={loreImage}
           alt="About page background image"
