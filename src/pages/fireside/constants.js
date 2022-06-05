c37f879dc85b77d525ff5a6268c7da838fbd87dc
Roberto Varela <rvarela@utexas.edu>
2022-06-05 11:16:40 -0500

    Added TAB_CONTENT

--- src/pages/fireside/constants.js
+++ src/pages/fireside/constants.js
@@ -12,3 +12,55 @@ export const TAB_LABELS = [
     label: <><span>More</span>Additional Explorations</>
   },
 ];
+
+export const TAB_CONTENT = [
+  {
+    id: 0,
+    header: <h4>About Us</h4>,
+    content: [
+      {
+        summary: <p>Camp Cosmos is a collection of 10,000 Campers living on the blockchain as ERC-721 tokens. Holding a Camper will provide you with access to the world of Camp Cosmos, a Web3 entertainment brand focused on storytelling and art from the visionary mind of Zutto. Your journey through the Cosmos begins with IRL events, airdrops, physical merchandise, brand partnerships and more.</p>,
+      }
+    ],
+  },
+  {
+    id: 1,
+    header: <h4>The Future</h4>,
+    content: [
+      {
+        label: <h6>Entertainment Conten</h6>,
+        summary: <p>We will develop various projects that will engage our current audience, as well as attract a mainstream audience to our brand and introduce new collectors to web3. Some of our targets are a children's book series, graphic novels, and an animated web/tv series.</p>,
+      },
+      {
+        label: <h6>IRL Events</h6>,
+        summary: <p>Our goal is to host various in real life events, with the main events being summer camps. These camps will be both adult and youth friendly, and will have an emphasis on both web3 and outdoor education. We also plan on hosting smaller, NFT focused events throughout the year, as well as educational workshops and art shows.</p>,
+      },
+      {
+        label: <h6>Merch and Collectibles</h6>,
+        summary: <p>We plan on releasing various physical merchandise and collectibles in the months following launch. Some of the items we'll offer are: Plush dolls, bobbleheads, clothing, camp gear, and more.</p>,
+      },
+      {
+        label: <h6>Brand Partnerships</h6>,
+        summary:  <p>Our team will utilize the connections we've made from over 10 years in the field of freelance art, photography, filmmaking and event planning. We're connected with many of the most prominent outdoor brands such as REI, Coleman, The North Face and more. We will also capitalize on Zutto's relationship with other brands who align with Camp Cosmos, such as Cartoon Network, NASA, and Adobe.</p>,
+      },
+    ],
+  },
+  {
+    id: 2,
+    header: <h4>Additional Explorations</h4>,
+    content: [
+      {
+        label: <h6>Web3 Activations</h6>,
+        summary: <p>Following launch, we will continue to work closely with West Coast NFT to expand Camp Cosmos within web3. You can expect future airdrops utilizing innovative mechanics such as wrapped NFTs and dynamic ERC-721 tokens (@transientlabs), NFT versions of entertainment media such as tokenized books, graphic novels, and animated shorts, as well as other blockchain activations.</p>,
+      },
+      {
+        label: <h6>Webstore and gallery</h6>,
+        summary: <p>We plan on developing a store within our website, where users can purchase NFTs, physical and digital merchandise, participate in community activations, and more. We have a talented team of front end developers (formerly IBM/Google) who are currently developing our website and a gallery similar to the one found on Azuki's website.</p>,
+      },
+      {
+        label: <h6>Growing the team</h6>,
+        summary: <p>It is vital that we work to scale Camp Cosmos by adding people who are experts in the fields that we aim to explore. This is including but not limited to expanding our marketing, business development, development, and design teams.</p>,
+      },
+    ],
+  },
+]
