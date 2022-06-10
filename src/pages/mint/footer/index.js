ad70cb146e9a185ffc78a840b15af1b15fe9577c
Roberto Varela <rvarela@utexas.edu>
2022-06-09 20:40:06 -0500

    Added <MintFooter />

---
+++ src/pages/mint/footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useAppContext } from '../context';
+import { META_MASK_STATUS } from '../context/constants';
+
+const MintFooter = () => {
+  const { metaMaskData: { address, status } } = useAppContext();
+
+  return (
+    <footer className='mint-footer'>
+      {
+        (META_MASK_STATUS.connected === status)
+          ? <button className='f-button' type='button'>{`CONNECTED TO: ${address.substring(0, 5)}...${address.substring(((address.length - 1) - 4), (address.length - 1))}`}</button>
+          : <p>1 CAMP COSMOS = 0.1 ETH</p>
+      }
+    </footer>
+  )
+};
+
+export default MintFooter;
--- src/pages/mint/index.js
+++ src/pages/mint/index.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import Layout from '../../components/layout';
 import PageBGImg from '../../components/page-bg-img';
+import MintFooter from './footer';
 import backgroundImage from '../../../public/images/pages-background/mint.png';
 import { ROLES } from './constants';
+// NOTE: MintContextProvider is for testing ONLY. Remove once connected to endpoint/contract
+import { MintContextProvider } from './context';
 
 const userRole = 'explorers';
 
 const Mint = () => (
-  <Layout>
-    <div className="mint page-layout">      
-      <div className="mint--content">
-        <div className="mint--content-summary">
-          <h2>Welcome To The <span>{ROLES[userRole]} Pre-Sale</span></h2>
-          <p>Counter</p>
-          <p>Button</p>
+  <MintContextProvider>
+    <Layout>
+      <div className="mint page-layout">
+        <div className="mint--content">
+          <div className="mint--content-summary">
+            <h2>Welcome To The <span>{ROLES[userRole]} Pre-Sale</span></h2>
+            <p>Counter</p>
+            <p>Button</p>
+          </div>
+          <MintFooter />
         </div>
-        <footer>Footer</footer>
       </div>
-    </div>
-    <PageBGImg
-      alt="Mint page background image"
-      src={backgroundImage}
-    />
-  </Layout>
+      <PageBGImg
+        alt="Mint page background image"
+        src={backgroundImage}
+      />
+    </Layout>
+  </MintContextProvider>
 );
 
 export default Mint;
